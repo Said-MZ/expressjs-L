e46776c3812ee8f4c0f70c8d034167890d96fe93
Said-MZ <saidzain897@gmail.com>
2025-03-15 17:56:33 +0300

    Implement user authentication and job management features

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,20 +1,26 @@
 require("dotenv").config();
 require("express-async-errors");
-const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
+
+// connect to db
+const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const authenticationMiddleware = require("./middleware/authentication");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticationMiddleware, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
